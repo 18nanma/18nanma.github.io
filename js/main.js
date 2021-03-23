d06c5071a0f03e4224b9d48a374e17584cbca1e0
18nanma <nanma.cs18@bmsce.ac.in>
2021-03-23 19:07:51 +0530

    responsive screen

--- js/main.js
+++ js/main.js
@@ -19,4 +19,29 @@
             aboutSection.querySelector(target).classList.add("active");
         }
     })
+
+    window.location.hash = hash;
 })();
+
+// navigation menu
+// (() => {
+//     const hamburgerBtn = document.querySelector(".hamburger-btn"),
+//     navMenu = document.querySelector(".nav-menu"),
+//     closeNavBtn = navMenu.querySelector(".close-nav-menu");
+    
+//     hamburgerBtn.addEventListener("click", showNavMenu);
+//     closeNavBtn.addEventListener("click", hideNavMenu);
+
+//     function showNavMenu(){
+//         navMenu.classList.add("open");
+//     }
+//     function hideNavMenu(){
+//         navMenu.classList.remove("open");
+//     }
+
+//     document.addEventListener('click', (event) => {
+//         if(event.target.classList.contains('link-item')){
+//             alert("hi")
+//         }
+//     })
+// })();
